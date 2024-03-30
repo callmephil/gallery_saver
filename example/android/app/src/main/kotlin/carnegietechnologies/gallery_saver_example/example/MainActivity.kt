156991ecf5dca6692bd2dfb37f1f7d80ddf4ceaa
Phil <phil@polarstork.com>
2024-03-30 16:55:30 +0200

    pub upgrade

---
+++ example/android/app/src/main/kotlin/carnegietechnologies/gallery_saver_example/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package carnegietechnologies.gallery_saver_example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -76,46 +76,62 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _takePhoto() async {
-    ImagePicker()
-        .getImage(source: ImageSource.camera)
-        .then((PickedFile recordedImage) {
-      if (recordedImage != null && recordedImage.path != null) {
-        setState(() {
-          firstButtonText = 'saving in progress...';
-        });
-        GallerySaver.saveImage(recordedImage.path, albumName: albumName)
-            .then((bool success) {
-          setState(() {
-            firstButtonText = 'image saved!';
-          });
-        });
-      }
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (pickedImage == null || pickedImage.path.isEmpty) {
+      return;
+    }
+
+    if (!mounted) return;
+    setState(() {
+      secondButtonText = 'saving in progress...';
     });
+
+    final result =
+        await GallerySaver.saveImage(pickedImage.path, albumName: albumName);
+
+    if (result case final success?) {
+      if (!mounted) return;
+
+      setState(() {
+        firstButtonText = success ? 'image saved!' : 'failed to save image';
+      });
+    }
   }
 
   void _recordVideo() async {
-    ImagePicker()
-        .getVideo(source: ImageSource.camera)
-        .then((PickedFile recordedVideo) {
-      if (recordedVideo != null && recordedVideo.path != null) {
-        setState(() {
-          secondButtonText = 'saving in progress...';
-        });
-        GallerySaver.saveVideo(recordedVideo.path, albumName: albumName)
-            .then((bool success) {
-          setState(() {
-            secondButtonText = 'video saved!';
-          });
-        });
-      }
+    final pickedVideo = await ImagePicker().pickVideo(
+      source: ImageSource.camera,
+    );
+
+    if (pickedVideo == null || pickedVideo.path.isEmpty) {
+      return;
+    }
+
+    if (!mounted) return;
+    setState(() {
+      secondButtonText = 'saving in progress...';
     });
+
+    final result =
+        await GallerySaver.saveVideo(pickedVideo.path, albumName: albumName);
+
+    if (result case final success?) {
+      if (!mounted) return;
+
+      setState(() {
+        secondButtonText = success ? 'video saved!' : 'failed to save video';
+      });
+    }
   }
 
   // ignore: unused_element
   void _saveNetworkVideo() async {
     String path =
         'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4';
-    GallerySaver.saveVideo(path, albumName: albumName).then((bool success) {
+    GallerySaver.saveVideo(path, albumName: albumName).then((bool? success) {
       setState(() {
         print('Video is saved');
       });
@@ -126,7 +142,7 @@ class _MyAppState extends State<MyApp> {
   void _saveNetworkImage() async {
     String path =
         'https://image.shutterstock.com/image-photo/montreal-canada-july-11-2019-600w-1450023539.jpg';
-    GallerySaver.saveImage(path, albumName: albumName).then((bool success) {
+    GallerySaver.saveImage(path, albumName: albumName).then((bool? success) {
       setState(() {
         print('Image is saved');
       });
@@ -171,12 +187,12 @@ class _ScreenshotWidgetState extends State<ScreenshotWidget> {
     });
     try {
       //extract bytes
-      final RenderRepaintBoundary boundary =
-          _globalKey.currentContext.findRenderObject();
+      final RenderRepaintBoundary boundary = _globalKey.currentContext!
+          .findRenderObject() as RenderRepaintBoundary;
       final ui.Image image = await boundary.toImage(pixelRatio: 3.0);
-      final ByteData byteData =
+      final ByteData? byteData =
           await image.toByteData(format: ui.ImageByteFormat.png);
-      final Uint8List pngBytes = byteData.buffer.asUint8List();
+      final Uint8List pngBytes = byteData?.buffer.asUint8List() ?? Uint8List(0);
 
       //create file
       final String dir = (await getApplicationDocumentsDirectory()).path;
